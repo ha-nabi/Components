0dbc0f0315e4619004830918e1475ac8c87ce67f
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-05 21:38:51 +0900

    Initial Commit

---
+++ Components/ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsApp.swift
+//  Components
+//
+//  Created by 강치우 on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
