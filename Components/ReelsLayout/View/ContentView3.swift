e5318fe5ef3f645b8865013f18b108e494c2c492
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-16 01:02:44 +0900

    Add: SetUp File

---
+++ Components/ReelsLayout/View/ContentView3.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView3.swift
+//  Components
+//
+//  Created by 강치우 on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView3()
+}
