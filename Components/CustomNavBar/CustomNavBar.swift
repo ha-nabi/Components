0dec7335319f7e69c8bf921686a676b9255d2d54
ciu <112779139+kangciu@users.noreply.github.com>
2023-12-11 11:45:39 +0900

    Docs: 폴더링

--- Components/CustomNavBar/CustomNavBar.swift
+++ Components/CustomNavBar/CustomNavBar.swift
@@ -7,13 +7,6 @@
 
 import SwiftUI
 
-struct scrollPreKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
-
 struct CustomNavBar: View {
     
     @State var isScroll: Bool = false
@@ -21,31 +14,7 @@ struct CustomNavBar: View {
     var body: some View {
         ScrollView {
             getScrollOffsetReader()
-            ForEach(0..<5) { item in
-                RoundedRectangle(cornerRadius: 30, style: .continuous)
-                    .foregroundStyle(.gray)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 200)
-                    .shadow(color: .black.opacity(0.2), radius: 10, x:0, y: 10)
-                    .padding()
-                    .overlay {
-                        VStack(alignment: .leading) {
-                            Circle()
-                                .frame(width: 50, height: 50)
-                            RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                .frame(width: 100, height: 25)
-                            VStack(alignment: .leading) {
-                                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                    .frame(width: 250, height: 15)
-                                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                    .frame(width: 200, height: 15)
-                                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                    .frame(width: 290, height: 15)
-                            }
-                            .padding(.top)
-                        }
-                    }
-            }
+            CardView()
         }
         .coordinateSpace(name: "scroll")
         .onPreferenceChange(scrollPreKey.self, perform: { value in
@@ -56,27 +25,20 @@ struct CustomNavBar: View {
                 .frame(height: 30)
         })
         .overlay {
-            ZStack {
-                Color.clear
-                    .frame(height: isScroll ? 90 : 100)
-                    .background(.ultraThinMaterial)
-                    .opacity(isScroll ? 1 : 0)
-                    .blur(radius: 0.5)
-                    .ignoresSafeArea()
-                // MARK: - 네비게이션 타이틀
-                HStack {
-                    Text("HomeView")
-                        .bold()
-                        .font(.system(size: isScroll ? 22 : 32))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Image(systemName: "person.circle.fill")
-                        .font(isScroll ? .title : .largeTitle)
-                }
-                .offset(y: isScroll ? -30 : -25)
-                .padding(.horizontal)
-            }
-            .frame(maxHeight: .infinity, alignment: .top)
+            VanDarView(isScroll: $isScroll)
+        }
+    }
+    
+    struct scrollPreKey: PreferenceKey {
+        static var defaultValue: CGFloat = 0
+        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+            value = nextValue()
+        }
+    }
+    
+    func updateScrollStatus(value: CGFloat) {
+        withAnimation(.default) {
+            isScroll = value < 0
         }
     }
     
@@ -86,15 +48,12 @@ struct CustomNavBar: View {
         }
     }
     
-    func updateScrollStatus(value: CGFloat) {
-        withAnimation(.default) {
-            isScroll = value < 0
-        }
-    }
 }
-
-
 
 #Preview {
     CustomNavBar()
 }
+
+
+
+
---
+++ Components/CustomNavBar/VanDarView.swift
@@ -0,0 +1,35 @@
+//
+//  VanDarView.swift
+//  Components
+//
+//  Created by 강치우 on 12/11/23.
+//
+
+import SwiftUI
+
+struct VanDarView: View {
+    @Binding var isScroll: Bool
+    var body: some View {
+        ZStack {
+            Color.clear
+                .frame(height: isScroll ? 90 : 100)
+                .background(.ultraThinMaterial)
+                .opacity(isScroll ? 1 : 0)
+                .blur(radius: 0.5)
+                .ignoresSafeArea()
+            // MARK: - 네비게이션 타이틀
+            HStack {
+                Text("HomeView")
+                    .bold()
+                    .font(.system(size: isScroll ? 22 : 32))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Image(systemName: "person.circle.fill")
+                    .font(isScroll ? .title : .largeTitle)
+            }
+            .offset(y: isScroll ? -30 : -25)
+            .padding(.horizontal)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+    }
+}
