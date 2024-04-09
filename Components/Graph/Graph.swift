1736c89f58c30abc9ef4cbc8366aaadb2b90fbeb
kangciu <a2849535@gmail.com>
2024-04-09 16:24:04 +0900

    파일 수정

---
+++ Components/Graph/Graph.swift
@@ -0,0 +1,18 @@
+//
+//  Graph.swift
+//  Components
+//
+//  Created by 강치우 on 4/10/24.
+//
+
+import SwiftUI
+
+struct Graph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Graph()
+}
