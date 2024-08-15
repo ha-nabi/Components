d77a6e06581debb2a0696fba25071c48166afffb
kangciu <a2849535@gmail.com>
2024-08-15 15:39:49 +0900

    add; FloatingBottomSheet

---
+++ Components/FloatingBottomSheet/FloatingBottomSheet.swift
@@ -0,0 +1,18 @@
+//
+//  FloatingBottomSheet.swift
+//  Components
+//
+//  Created by 강치우 on 8/16/24.
+//
+
+import SwiftUI
+
+struct FloatingBottomSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FloatingBottomSheet()
+}
